8a1944e652858d92307e968189d1cafc95cd883a
Jorge Castro <me@jorgecastro.xyz>
2023-01-02 06:31:32 -0800

    Ensure board size argument is not less than 1 and change output format

--- eight_queens_puzzle/main/__main__.py
+++ eight_queens_puzzle/main/__main__.py
@@ -3,12 +3,13 @@ import sys
 from .eight_queens_puzzle import Chessboard, solve
 
 
-size = int(sys.argv[1]) if len(sys.argv) >= 2 else 8
+size = int(sys.argv[1]) if len(sys.argv) >= 2 and int(sys.argv[1]) >= 1 else 8
 chessboard = Chessboard(size)
 
 for i, solution in enumerate(solve(chessboard), start=1):
-    print("*" * 20)
-    print(f"Solution number: {i}")
+    print("*" * (len(solution) * 2))
+    print(f"Solution #{i}")
+    print("*" * (len(solution) * 2))
     print(solution)
 
 sys.exit(0)
